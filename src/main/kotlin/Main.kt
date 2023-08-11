2a0f6a690bf53e2db201580c32dc48b02011f565
Jhon-Bj <jhon.bejarano@uniminuto.edu.co>
2023-08-10 19:14:59 -0500

    s

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,10 @@
+import java.lang.ref.PhantomReference
 import java.text.DecimalFormat
 import kotlin.math.ceil
 import kotlin.math.floor
 
-fun main(){
-/*
+fun main() {
+    /*
     println("hola kotlin")
 
     // TIPOS DE VARIABLES
@@ -113,7 +114,7 @@ fun main(){
     }
     println(z)
 
- */
+
     var f = 100
 
     do{
@@ -123,5 +124,87 @@ fun main(){
 
 
     }while (f>=1)
+
+
+
+    var numero = 2
+
+    when (numero){
+        1->
+            println("NO ES IGUAL")
+
+        2->{
+            println("NO ES IGUAL")
+        }
+        3->{
+            println("Si es igual")
+        }
+
+        else ->println("NUMERO NO ENCONTRADO")
+
+    var combustible = "gasolina"
+    when (combustible) {
+        "galonina", "diesel" -> println("NO ES UN CARRO ELECTRICO")
+        else -> println("EL CARRO ES ELECTRICO")
+    }
+
+    var edad =24
+    when (edad){
+        in 1.. 10 -> println("ES UN NIÑO")
+        in 11 .. 17 -> println("ES UN ADOLESCENTE")
+        in 18..29 -> println("ES UN ADULTO JOVEN")
+    }
+
+
+
+    //ARREGLOS
+    //arraitof: me permiten almacenar cualquier tipo de dato
+
+    var estudiantes = arrayOf("LORENA","PEDRO", "LUIS", 20,58,67)
+    println(estudiantes[4])
+
+    println(estudiantes.size)
+    *//*
+
+    var numeros = intArrayOf(12,45,67,19)
+    println(numeros[0])
+    println(numeros[1])
+    println(numeros[2])
+    println(numeros[3])
+
+
+    for (i in 0 .. numeros.size-1){
+        println(numeros[i])
+    }
+    */
+    //USO DE ARRAYLIST
+    //arrayListOf<String> La lista va a ser de tipo string
+    //Sino lo especifico el tipo de dato lo que sucede es que la lista recibe recibe cualquier tipo de dato
+
+    var milista =arrayListOf("Lorena","Pedro",28,58,41)
+
+    var num1:Int = milista[2] as Int
+    var num2:Int = milista[3] as Int
+
+    /* var nombre:String =milista[0] as String + milista[2]
+    println(nombre)
+
+    //println(num1 + num2)
+    */
+
+
+
+    milista.forEach {i, est ->
+        println("$i - $est")
+    }
+
+
+
+
+
+
+
+
+
 
 }
